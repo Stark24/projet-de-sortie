846ffea46bfe10e76a8a7870c1b744935113005c
Joseph Peterson <josephpeterson492@gmail.com>
2018-09-11 15:48:50 -0400

    ajout des classes data access object

--- OkapanmProjet/src/Entreprise/bean/EntrepriseDAO.java
+++ OkapanmProjet/src/Entreprise/bean/EntrepriseDAO.java
@@ -3,6 +3,9 @@
  */
 package Entreprise.bean;
 
+import java.sql.ResultSet;
+import java.util.Vector;
+
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 import connection.Connect_Base;
@@ -13,10 +16,12 @@ import connection.Connect_Base;
  * @author Windows 10
  *
  */
+// classe pour les methodes manipulant les entreprises
 public class EntrepriseDAO {
 	Connection con = (Connection) Connect_Base.getInstance();
  
-	public boolean inscrireUtilisateur(Entreprise e)
+	// methode pour enregistrer les entreprises
+	public boolean enregisterEntreprise(Entreprise e)
 	{
 		 try
 	     {
@@ -40,4 +45,73 @@ public class EntrepriseDAO {
 
 	     return false;
 	 }
+	
+	// methode pour mettre a jour une entreprise
+	
+	 public boolean update(Entreprise e)
+	    {
+	        try
+	        {
+	            String req = "UPDATE `okapanm_db`.`Entreprise` "
+	                    + "SET `nomEntreprise`='" + e.getNomEntreprise() + "', "
+	                    + "`Adresse`='" + e.getAdresse() + "', "
+	                    + "`telephone`='" + e.getTelephone() + "', "
+	                    + "`email`='" + e.getEmail() + "', "
+	                    + "`horaire`='" + e.getHoraire() + "' "
+	                    + "WHERE `nomEntreprise`='" + e.getNomEntreprise() + "';";
+
+	            java.sql.Statement st = con.createStatement();
+
+	            int rs = st.executeUpdate(req);
+
+	            if (rs > 0)
+	            {
+	                return true;
+	            }
+
+	        } catch (Exception e1)
+	        {
+	            // TODO Auto-generated catch block
+	            e1.printStackTrace();
+	        }
+
+	        return false;
+	    }
+	 
+	 // methode pour afficher toutes les entreprises
+	 
+	 
+	 public Vector<Entreprise> findAll()
+	    {
+	        String req = "SELECT * " + " FROM entreprise ";
+
+	        Vector<Entreprise> vect = null;
+
+	        try
+	        {
+	            java.sql.Statement st = con.createStatement();
+	            ResultSet rs = st.executeQuery(req);
+
+	            while (rs.next())
+	            {
+
+	                if (vect == null)
+	                {
+	                    vect = new Vector<>();
+	                }
+
+	                Entreprise e = new Entreprise(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+
+	                vect.add(e);
+	            }
+
+	            return vect;
+	        } catch (Exception e)
+	        {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+
+	        return null;
+	    }
 }
--- OkapanmProjet/src/user/UserDAo.java
+++ OkapanmProjet/src/user/UserDAo.java
@@ -3,18 +3,23 @@
  */
 package user;
 
+import java.sql.ResultSet;
+import java.util.Vector;
+
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
-
 import connection.Connect_Base;
 
 /**
  * @author Windows 10
  *
  */
+// classe pour les methodes sur les utilisateurs
 public class UserDAo {
 	
 	Connection con = (Connection) Connect_Base.getInstance();
+	
+	// la methode pour inscrire les utilisateurs
 public boolean inscrireUtilisateur(Utilisateur u)
 {
 	 try
@@ -39,6 +44,108 @@ public boolean inscrireUtilisateur(Utilisateur u)
 
      return false;
  }
+// methode pour mettre a jour les donnees des utilsateurs
 
- 
+public boolean update(Utilisateur u)
+{
+    try
+    {
+        String req = "UPDATE `okapanm_db`.`user` "
+                + "SET `nom`='" + u.getNom() + "', "
+                + "`prenom`='" + u.getPrenom() + "', "
+                + "`sexe`='" + u.getSexe() + "' "
+                + "`adresse`='" + u.getAdresse() + "', "
+                + "`telephone`='" + u.getTelephone() + "', "
+                + "`email`='" + u.getEmail() + "' "
+                + " `rue`='" + u.getRue() + "', "
+                + "`ville`='" + u.getVille() + "', "
+                + "`mdp`='" + u.getMdp() + "' "
+                + " `pseudo`='" + u.getPseudo() + "', "
+                + "`urlImage`='" + u.getUrlImage() + "', "
+                + "WHERE `pseudo`='" + u.getPseudo() + "';";
+
+       
+
+        java.sql.Statement st = con.createStatement();
+
+        int rs = st.executeUpdate(req);
+
+        if (rs > 0)
+        {
+            return true;
+        }
+
+    } catch (Exception e1)
+    {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+    }
+
+    return false;
 }
+
+// methode pour desactiver un utilisateur
+public boolean bloquer(String pseudo)
+{
+    try
+    {
+        String req = "UPDATE user SET "
+                + "etat=0"
+                + " WHERE pseudo='" + pseudo + "';";
+
+        java.sql.Statement st = con.createStatement();
+
+        int rs = st.executeUpdate(req);
+
+        if (rs > 0)
+        {
+            return true;
+        }
+
+    } catch (Exception e1)
+    {
+        // TODO Auto-generated catch block
+        e1.printStackTrace();
+    }
+
+    return false;
+}
+
+// methode pour afficher touts les utilisateurs
+public Vector<Utilisateur> findAll()
+{
+    String req = "SELECT * " + " FROM user ";
+
+    Vector<Utilisateur> vect = null;
+
+    try
+    {
+        java.sql.Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(req);
+
+        while (rs.next())
+        {
+
+            if (vect == null)
+            {
+                vect = new Vector<>();
+            }
+
+            Utilisateur e = new Utilisateur(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9), rs.getString(10), rs.getString(11) );
+
+            vect.add(e);
+        }
+
+        return vect;
+    } catch (Exception e)
+    {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+
+    return null;
+}
+
+
+}
+
